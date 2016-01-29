8277113a9412e7cdc317b0255c00ed720127d77c
juankboards <juankboards@hotmail.com>
2016-01-29 00:21:23 -0500

    correct layout

--- js/javascript.js
+++ js/javascript.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	$('.ball5').on({
         'click': function(){
             var imgId = "#" + this.id            
-            $(imgId).css("bottom") === "80px"? $(imgId).css("bottom", "53px"):$(imgId).css("bottom", "80px");
+            $(imgId).css("bottom") === "95px"? $(imgId).css("bottom", "53px"):$(imgId).css("bottom", "95px");
         }
     });
 
@@ -12,14 +12,14 @@ $(document).ready(function(){
         	var numId = idStr.slice(-1);
         	var imgId = "#" + idStr.slice(0, idStr.length-1) + numId;
 	        var baseBottomValue = $(".ball"+numId).css("bottom");
-	        var newBottomValue = (parseInt(baseBottomValue.slice(0, baseBottomValue.length-2)) + 36).toString() + "px";  
+	        var newBottomValue = (parseInt(baseBottomValue.slice(0, baseBottomValue.length-2)) + 78).toString() + "px";  
 
         	if ($(imgId).css("bottom") !== newBottomValue){
 	            for (var i = numId; i < 5; i++) {   
 
 	            	imgId = "#" + idStr.slice(0, idStr.length-1) + i;
 	            	baseBottomValue = $(".ball"+i).css("bottom");
-	            	newBottomValue = (parseInt(baseBottomValue.slice(0, baseBottomValue.length-2)) + 36).toString() + "px";   
+	            	newBottomValue = (parseInt(baseBottomValue.slice(0, baseBottomValue.length-2)) + 78).toString() + "px";   
 
 	            	if ($(imgId).css("bottom") !== newBottomValue){
 	            		$(imgId).css("bottom", newBottomValue); 
