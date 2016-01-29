c34c3e084efb7e155d5633a186eff4c6c672132a
juankboards <juankboards@hotmail.com>
2016-01-28 22:23:47 -0500

    add background, lower balls, movement to lower balls, correct layout

--- js/javascript.js
+++ js/javascript.js
@@ -1,16 +1,43 @@
-$(document).ready(function(){
-	//function moveFive(clicked_id){
-    //	$(clicked_id).css("bottom") === "84px"? $(clicked_id).css("bottom", "52px"):$(clicked_id).css("bottom", "84px");	
-	//}
-
-	$('.five').on({
+$(document).ready(function(){	
+	$('.ball5').on({
         'click': function(){
-            var imgId = "#" + this.id
-            console.log(imgId);
-            $(imgId).css("bottom") === "84px"? $(imgId).css("bottom", "52px"):$(imgId).css("bottom", "84px");
+            var imgId = "#" + this.id            
+            $(imgId).css("bottom") === "80px"? $(imgId).css("bottom", "53px"):$(imgId).css("bottom", "80px");
         }
     });
 
-	
+    $('.lower > img').on({
+        'click': function (){
+        	var idStr = this.id;
+        	var numId = idStr.slice(-1);
+        	var imgId = "#" + idStr.slice(0, idStr.length-1) + numId;
+	        var baseBottomValue = $(".ball"+numId).css("bottom");
+	        var newBottomValue = (parseInt(baseBottomValue.slice(0, baseBottomValue.length-2)) + 36).toString() + "px";  
+
+        	if ($(imgId).css("bottom") !== newBottomValue){
+	            for (var i = numId; i < 5; i++) {   
+
+	            	imgId = "#" + idStr.slice(0, idStr.length-1) + i;
+	            	baseBottomValue = $(".ball"+i).css("bottom");
+	            	newBottomValue = (parseInt(baseBottomValue.slice(0, baseBottomValue.length-2)) + 36).toString() + "px";   
+
+	            	if ($(imgId).css("bottom") !== newBottomValue){
+	            		$(imgId).css("bottom", newBottomValue); 
+	            	}            	
+	            } 
+	        } else{
+
+	            for (var i = numId; i > 0; i--) {   
+
+	            	imgId = "#" + idStr.slice(0, idStr.length-1) + i;
+	            	baseBottomValue = $(".ball"+i).css("bottom");	            	   
+
+	            	if ($(imgId).css("bottom") !== baseBottomValue){
+	            		$(imgId).css("bottom", baseBottomValue); 
+	            	}            	
+	            }    
+	        }           	
+        }            
+    });
 });
 
